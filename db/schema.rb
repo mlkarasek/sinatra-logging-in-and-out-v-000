b729712d4487acc8a77ea864abaee258ad164bcd
Monica  Karasek <mlkarasek@gmail.com>
2018-08-15 21:53:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180815042651) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.decimal "balance"
+    t.string "balance"
   end
 
 end
